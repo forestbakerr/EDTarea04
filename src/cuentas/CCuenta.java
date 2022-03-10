f687399dae452fd79bb6e3716d99607c795bbcf4
forestbakerr <101337296+forestbakerr@users.noreply.github.com>
2022-03-10 21:32:55 +0100

    Cambios de refactorización sh: Q: command not found

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -5,7 +5,12 @@
 
 package cuentas;
 
-
+/**
+ * Clase qeu define objetos de tipo cuenta bancaria y que define varios 
+ * métodos para manipular dichos objetos.
+ * @author Aiala Medrano 
+ * DAW ED_Tarea04
+ */
 public class CCuenta {
 
     /**
@@ -36,48 +41,89 @@ public class CCuenta {
         this.saldo = saldo;
     }
 
-
+    /**
+     * Nombre del propietario de la cuenta.
+     */
     private String nombre;
+    /**
+     * Nombre de la cuenta.
+     */
     private String cuenta;
+    /**
+     * Valor del total almacenado enla cuenta.
+     */
     private double saldo;
+    /**
+     * Valor del interés que genera la cuenta.
+     */
     private double tipoInterés;
 
-
+    /**
+     * Método constructor.
+     */
     public CCuenta()
     {
     }
-
+    /**
+     * Método constructor con parámetros especificados.
+     * @param nom Nombre del propietario de la cuenta.
+     * @param cue Nombre de la cuenta.
+     * @param sal Valor del total almacenado en la cuenta.
+     * @param tipo Valor del interés que genera la cuenta. Por algún motivo, 
+     * se pide este valor por parámetro, pero después el constructor no lo utiliza.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    /**
+     * Método que pide una cadena de caracteres por parámetro y la almacena en 
+     * el atributo nombre.
+     * @param nom 
+     */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-
+    /**
+     * Método que devuelve la cadena de caracteres almacenada en el atributo 
+     * nombre.
+     * @return devuelve una cadena de caracteres.
+     */
     public String obtenerNombre()
     {
         return getNombre();
     }
 
-
+    /**
+     * Método que devuelve el valor almacenado en el atributo saldo.
+     * @return Devuelve un valor de tipo double.
+     */
     public double estado()
     {
         return getSaldo();
     }
 
-
+    /**
+     * Método que suma un valor que entra por parámetro al atributo saldo.
+     * @param cantidad Valor que se quiere sumar al saldo.
+     * @throws Exception Si el método no puede operar con éxito lanza una 
+     * excepción.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * Método que resta un valor que entra por parámetro al atributo saldo.
+     * @param cantidad Valor que se quiere restar al saldo.
+     * @throws Exception Si el método no puede operar con éxito lanza una 
+     * excepción.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -87,28 +133,47 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
-
+    /** 
+     * Método que devuelve la cadena de caracteres almacenada en el atributo 
+     * cuenta.
+     * @return devuelve una cadena de caracteres.
+     */
     public String obtenerCuenta()
     {
         return getCuenta();
     }
 
-
+    /**
+     * Método que devuelve la cadena de caracteres almacenada en el atributo
+     * nombre.
+     * @return Devuelve una cadena de caracteres.
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     * Método que asigna una cadena de caracteres que entra por parámetro al 
+     * atributo nombre.
+     * @param nombre Nombre del propietario de la cuenta.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * Método que devuelve el valor almacenado en el atributo tipoInterés.
+     * @return Devuelve un valor tipo double.
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     * Método que asigna un valor tipo double que entra por parámetro al 
+     * atributo tipoInterés.
+     * @param tipoInterés Valor del tipo de interés que genera la cuenta.
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
